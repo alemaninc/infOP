3e75404ed0d3d672537678ae3fc15fe0fe7518bf
alemaninc <97172850+alemaninc@users.noreply.github.com>
2022-02-08 21:04:25 +0000

    Update indexCode.js

--- indexCode.js
+++ indexCode.js
@@ -2,14 +2,9 @@ var tabOpen="Introduction"
 function openTab(name) {
   tabOpen=name
 }
-function openTab(name) {
-  var i;
-  var x = document.getElementsByClassName("tab");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  document.getElementById(name).style.display = "inline-block";  
-}
 window.setInterval(function(){
+  document.getElementById("buttonIntroduction").style.display = (tabOpen=="Introduction") ? "buttonon" : "buttonoff";
   document.getElementById("Introduction").style.display = (tabOpen=="Introduction") ? "inline-block" : "none";
+  document.getElementById("buttonFundamentalFunctions").style.display = (tabOpen=="Fundamental Functions") ? "buttonon" : "buttonoff";
+  document.getElementById("Fundamental Functions").style.display = (tabOpen=="Fundamental Functions") ? "inline-block" : "none";
 }, 50);
