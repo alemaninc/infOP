4bd5feee17d6995cc02832d93803a367320b32f5
alemaninc <97172850+alemaninc@users.noreply.github.com>
2022-02-08 19:47:39 +0000

    Added function for button

--- indexCode.js
+++ indexCode.js
@@ -1,1 +1,15 @@
-
+var tabOpen="Introduction"
+function openTab(name) {
+  tabOpen=name
+}
+function openTab(name) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(name).style.display = "inline-block";  
+}
+window.setInterval(function(){
+  document.getElementById("Introduction").style.display = (tabOpen=="Introduction") ? "inline-block" : "none";
+}, 50);
