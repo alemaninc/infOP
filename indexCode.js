d492cebed58161bc57b51116fd83102ac668d0bf
alemaninc <97172850+alemaninc@users.noreply.github.com>
2022-02-10 20:14:17 +0000

    Update indexCode.js

--- indexCode.js
+++ indexCode.js
@@ -7,4 +7,12 @@ window.setInterval(function(){
   document.getElementById("Introduction").style.display = (tabOpen==1) ? "inline-block" : "none";
   document.getElementById("buttonFundamentalFunctions").className = (tabOpen==2) ? "buttonon" : "buttonoff";
   document.getElementById("Fundamental Functions").style.display = (tabOpen==2) ? "inline-block" : "none";
+  document.getElementById("buttonSoftcaps").className = (tabOpen==3) ? "buttonon" : "buttonoff";
+  document.getElementById("Softcaps").style.display = (tabOpen==3) ? "inline-block" : "none";
+  document.getElementById("buttonScalings").className = (tabOpen==4) ? "buttonon" : "buttonoff";
+  document.getElementById("Scalings").style.display = (tabOpen==4) ? "inline-block" : "none";
+  document.getElementById("buttonMiscellaneous").className = (tabOpen==-1) ? "buttonon" : "buttonoff";
+  document.getElementById("Miscellaneous").style.display = (tabOpen==-1) ? "inline-block" : "none";
+  document.getElementById("buttonChangelog").className = (tabOpen==-2) ? "buttonon" : "buttonoff";
+  document.getElementById("Changelog").style.display = (tabOpen==-2) ? "inline-block" : "none";
 }, 50);
