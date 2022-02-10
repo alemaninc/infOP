db5038542a15df3f35a7b6c1c561c9993cc29c8b
alemaninc <97172850+alemaninc@users.noreply.github.com>
2022-02-10 07:19:59 +0000

    Update indexCode.js

--- indexCode.js
+++ indexCode.js
@@ -3,8 +3,8 @@ function openTab(name) {
   tabOpen=name
 }
 window.setInterval(function(){
-  document.getElementById("buttonIntroduction").style.display = (tabOpen=="Introduction") ? "buttonon" : "buttonoff";
+  document.getElementById("buttonIntroduction").className = (tabOpen=="Introduction") ? "buttonon" : "buttonoff";
   document.getElementById("Introduction").style.display = (tabOpen=="Introduction") ? "inline-block" : "none";
-  document.getElementById("buttonFundamentalFunctions").style.display = (tabOpen=="Fundamental Functions") ? "buttonon" : "buttonoff";
+  document.getElementById("buttonFundamentalFunctions").className = (tabOpen=="Fundamental Functions") ? "buttonon" : "buttonoff";
   document.getElementById("Fundamental Functions").style.display = (tabOpen=="Fundamental Functions") ? "inline-block" : "none";
 }, 50);
